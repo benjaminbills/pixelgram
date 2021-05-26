aef74389fbd266e9b73aa3fd608b85999caa5dcb
Benjamin Obafemi <benjaminoba234@gmail.com>
2021-05-26 08:01:19 +0300

    add welcome mail files

--- pixels/email.py
+++ pixels/email.py
@@ -7,8 +7,8 @@ def send_welcome_email(name,receiver):
     sender = 'oneobaben@gmail.com'
 
     #passing in the context vairables
-    text_content = render_to_string('email/newsemail.txt',{"name": name})
-    html_content = render_to_string('email/newsemail.html',{"name": name})
+    text_content = render_to_string('email/welcomemail.txt',{"name": name})
+    html_content = render_to_string('email/welcomemail.html',{"name": name})
 
     msg = EmailMultiAlternatives(subject,text_content,sender,[receiver])
     msg.attach_alternative(html_content,'text/html')
