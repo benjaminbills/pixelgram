68cd2bca1bdeeef1dad79600d84bb7d354188fcb
Benjamin Obafemi <benjaminoba234@gmail.com>
2021-05-26 09:12:10 +0300

    add settings

--- pixelgram/settings.py
+++ pixelgram/settings.py
@@ -80,20 +80,20 @@ MODE=config("MODE", default="dev")
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG')
 # development
-if config('MODE')=="dev":
-   DATABASES = {
-       'default': {
-           'ENGINE': 'django.db.backends.postgresql_psycopg2',
-           'NAME': config('DB_NAME'),
-           'USER': config('DB_USER'),
-           'PASSWORD': config('DB_PASSWORD'),
-           'HOST': config('DB_HOST'),
-           'PORT': '',
-       }
+# if config('MODE')=="dev":
+#    DATABASES = {
+#        'default': {
+#            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#            'NAME': config('DB_NAME'),
+#            'USER': config('DB_USER'),
+#            'PASSWORD': config('DB_PASSWORD'),
+#            'HOST': config('DB_HOST'),
+#            'PORT': '',
+#        }
        
-   }
-else:
-   DATABASES = {
+#    }
+# else:
+DATABASES = {
        'default': dj_database_url.config(
            default=config('DATABASE_URL')
        )
